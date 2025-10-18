e95b4dfce3989e12abfc7c2819d3841c11bad689
Matthew <indetermattrix@gmail.com>
2025-10-19 01:04:00 +0300

    add data from prev lab

--- lab_2/lab_2.sql
+++ lab_2/lab_2.sql
@@ -37,3 +37,24 @@ CREATE TABLE auth_user(
     fio        text NOT NULL UNIQUE REFERENCES lab2.employees(fio)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+-- данные из прошлой лабы
+INSERT INTO lab2.employees(fio) VALUES
+  ('Alice'), ('Bob'), ('Cristian'), ('Denis');
+
+INSERT INTO lab2.departments(dept_name, dept_no, head_fio, positions_total, wage_fund, positions_occupied) VALUES
+  ('Отдел 1', 1, 'Alice', 5.00, 1000000.00, 4.00),
+  ('Отдел 2', 2, 'Bob',   7.00, 2500000.00, 6.00);
+
+INSERT INTO lab2.employment(fio, dept_name, dept_no, share, position, descriptor) VALUES
+  ('Cristian','Отдел 1', 1, 0.60, 'Инженер',          'Backend'),
+  ('Cristian','Отдел 2', 2, 0.40, 'QA-аналитик',      'Автотесты'),
+  ('Alice',   'Отдел 1', 1, 1.00, 'Начальник отдела', 'Руководство'),
+  ('Bob',     'Отдел 2', 2, 1.00, 'Начальник отдела', 'Руководство'),
+  ('Denis',   'Отдел 1', 1, 1.00, 'Инженер',          'Frontend');
+
+INSERT INTO lab2.auth_user(login_name, fio) VALUES
+  ('alice',   'Alice'),
+  ('bob',     'Bob'),
+  ('cristian','Cristian'),
+  ('denis',   'Denis');
