61665b96eb0506b80232fdd933717b98fc16687d
Matthew <indetermattrix@gmail.com>
2025-10-19 13:23:33 +0300

    function and trigger

--- lab_2/lab_2.sql
+++ lab_2/lab_2.sql
@@ -4,38 +4,38 @@ SET search_path = lab2;
 
 --- таблицы ---
 CREATE TABLE employees(
-    fio TEXT PRIMARY KEY
+  fio TEXT PRIMARY KEY
 );
 
 CREATE TABLE departments(
-    dept_name TEXT NOT NULL,
-    dept_no   INTEGER NOT NULL,
-    head_fio  TEXT NOT NULL REFERENCES employees(fio),
-    positions_total NUMERIC(6,2) NOT NULL,
-    wage_fund       NUMERIC(14,2) NOT NULL,
-    positions_occupied NUMERIC(6,2) NOT NULL,
-    CONSTRAINT pk_departments PRIMARY KEY (dept_name),
-    CONSTRAINT ak1_departments UNIQUE (dept_no, head_fio)
+  dept_name TEXT NOT NULL,
+  dept_no   INTEGER NOT NULL,
+  head_fio  TEXT NOT NULL REFERENCES employees(fio),
+  positions_total NUMERIC(6,2) NOT NULL,
+  wage_fund       NUMERIC(14,2) NOT NULL,
+  positions_occupied NUMERIC(6,2) NOT NULL,
+  CONSTRAINT pk_departments PRIMARY KEY (dept_name),
+  CONSTRAINT ak1_departments UNIQUE (dept_no, head_fio)
 );
 
 ALTER TABLE departments ADD CONSTRAINT ak_dept_name_no UNIQUE (dept_name, dept_no);
 
 CREATE TABLE employment(
-    fio        TEXT NOT NULL REFERENCES employees(fio),
-    dept_name  TEXT NOT NULL,
-    dept_no    INTEGER NOT NULL,
-    share      NUMERIC(6,2) NOT NULL CHECK (share > 0 AND share <= 1),
-    position   TEXT NOT NULL,
-    descriptor TEXT NOT NULL,
-    CONSTRAINT fk1_employment FOREIGN KEY (dept_name, dept_no)
-        REFERENCES departments(dept_name, dept_no),
-    CONSTRAINT pk_employment PRIMARY KEY (fio, dept_no, position)
+  fio        TEXT NOT NULL REFERENCES employees(fio),
+  dept_name  TEXT NOT NULL,
+  dept_no    INTEGER NOT NULL,
+  share      NUMERIC(6,2) NOT NULL CHECK (share > 0 AND share <= 1),
+  position   TEXT NOT NULL,
+  descriptor TEXT NOT NULL,
+  CONSTRAINT fk1_employment FOREIGN KEY (dept_name, dept_no)
+    REFERENCES departments(dept_name, dept_no),
+  CONSTRAINT pk_employment PRIMARY KEY (fio, dept_no, position)
 );
 
 CREATE TABLE auth_user(
-    login_name text PRIMARY KEY,
-    fio        text NOT NULL UNIQUE REFERENCES lab2.employees(fio)
-        ON UPDATE CASCADE ON DELETE CASCADE
+  login_name text PRIMARY KEY,
+  fio        text NOT NULL UNIQUE REFERENCES lab2.employees(fio)
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- данные из прошлой лабы
@@ -58,3 +58,49 @@ INSERT INTO lab2.auth_user(login_name, fio) VALUES
   ('bob',     'Bob'),
   ('cristian','Cristian'),
   ('denis',   'Denis');
+
+-- ограничение и триггер--
+-- функция проверки
+CREATE OR REPLACE FUNCTION lab2.check_share_limit()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  v_is_head   boolean;
+  v_sum_share numeric(8,2);
+  v_limit     numeric(8,2);
+BEGIN
+  SELECT EXISTS (
+    SELECT 1 FROM lab2.departments d WHERE d.head_fio = NEW.fio
+  )
+  INTO v_is_head;
+
+  v_limit := CASE WHEN v_is_head THEN 1.00 ELSE 1.50 END;
+
+  SELECT COALESCE(SUM(e.share), 0)
+    INTO v_sum_share
+    FROM lab2.employment e
+   WHERE e.fio = NEW.fio
+     AND (e.fio, e.dept_no, e.position)
+         <> (COALESCE(OLD.fio, '§'), COALESCE(OLD.dept_no, -1), COALESCE(OLD.position, '§'));
+
+  v_sum_share := v_sum_share + COALESCE(NEW.share, 0);
+
+  IF v_sum_share > v_limit THEN
+    RAISE EXCEPTION
+      'у % суммарная ставка = %, лимит = %',
+      NEW.fio, v_sum_share, v_limit
+      USING ERRCODE = 'check_violation';
+  END IF;
+
+  RETURN NEW;
+END;
+$$;
+
+-- триггер
+DROP TRIGGER IF EXISTS trg_check_share_limit ON lab2.employment;
+CREATE TRIGGER trg_check_share_limit
+BEFORE INSERT OR UPDATE OF fio, share
+ON lab2.employment
+FOR EACH ROW
+EXECUTE FUNCTION lab2.check_share_limit();
